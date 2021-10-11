654db450fdefbfd264c70dcd6fa23094e59c82c0
mharizanova8703 <mharizanova@yahoo.com>
2021-10-11 16:22:01 -0400

    creating 5 quiz questions

---
+++ app.js
@@ -0,0 +1,34 @@
+
+//quiz questions
+var questions = [
+  {
+    title: 'Inside which HTML element do we put the JavaScript?',
+    choices: ['scripting', 'script', 'js', 'javascript'],
+    answer: 'script',
+  },
+  {
+    title:
+      'Which of the following function of String object combines the text of two strings and returns a new string?',
+    choices: ['add( )', 'concat( )', ' merge( )', 'append( )'],
+    answer: 'concat( )',
+  },
+
+{
+    title: "Where is the correct place to insert a JavaScript?" ,
+    choices:["The <head> section", "The <body> section", "Both the <head> section and the <body> section are correct",],
+    answer: "Both the <head> section and the <body> section are correct",
+
+
+},
+{
+title:  "How do you create a function in JavaScript?",
+choices:   "function myFunction()", "function:myFunction()", "function = myFunction()",
+answer: "function = myFunction()" ,
+},
+{
+    title: "Which of the following function of an array object adds and/or removes elements from an array?",
+        choices: ["toSource( )", "sort( )", "unshift( )", "splice( )"],
+        answer: "splice( )"
+}
+
+]
